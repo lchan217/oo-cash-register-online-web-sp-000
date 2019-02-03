d0d93b7ecee4a31877cee370cb9b7e1790027989
Lizzie Chan <lchan217@gmail.com>
2019-02-03 23:06:59 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -3,11 +3,13 @@ class CashRegister
   def initialize(discount=0)
     @total = 0
     @discount = discount 
+    @cart = []
   end 
   def add_item(title, price, quantity=0)
     @total += price*quantity
     i = 0 
     while quantity > i 
-      
+    @cart << title 
+    i+= 1
   end 
 end 
